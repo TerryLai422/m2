a07c6ba717b6cc3fe6b145e5226672bbc82e07bd
TerryLai422 <35938314+TerryLai422@users.noreply.github.com>
2024-11-24 01:44:47 -0500

    indicators

---
+++ m2-script/questdb/insert_into_indicators.sql
@@ -0,0 +1,64 @@
+INSERT INTO indicators
+SELECT 
+    TICKER, 
+    'MA_5', 
+    DATE, 
+    CLOSE, 
+    avg(close) 
+        OVER 
+        (PARTITION BY TICKER, PER ORDER BY DATE 
+        ROWS BETWEEN UNBOUNDED PRECEDING AND 4 PRECEDING) 
+        AS "VALUE" 
+FROM historial_d
+
+INSERT INTO indicators
+SELECT 
+    TICKER, 
+    'MA_20', 
+    DATE, 
+    CLOSE, 
+    avg(close) 
+        OVER 
+        (PARTITION BY TICKER, PER ORDER BY DATE 
+        ROWS BETWEEN UNBOUNDED PRECEDING AND 19 PRECEDING) 
+        AS "VALUE" 
+FROM historial_d
+
+INSERT INTO indicators
+SELECT 
+    TICKER, 
+    'MA_50', 
+    DATE, 
+    CLOSE, 
+    avg(close) 
+        OVER 
+        (PARTITION BY TICKER, PER ORDER BY DATE 
+        ROWS BETWEEN UNBOUNDED PRECEDING AND 49 PRECEDING) 
+        AS "VALUE" 
+FROM historial_d
+
+INSERT INTO indicators
+SELECT 
+    TICKER, 
+    'MA_150', 
+    DATE, 
+    CLOSE, 
+    avg(close) 
+        OVER 
+        (PARTITION BY TICKER, PER ORDER BY DATE 
+        ROWS BETWEEN UNBOUNDED PRECEDING AND 149 PRECEDING) 
+        AS "VALUE" 
+FROM historial_d
+
+INSERT INTO indicators
+SELECT 
+    TICKER, 
+    'MA_200', 
+    DATE, 
+    CLOSE, 
+    avg(close) 
+        OVER 
+        (PARTITION BY TICKER, PER ORDER BY DATE 
+        ROWS BETWEEN UNBOUNDED PRECEDING AND 199 PRECEDING) 
+        AS "VALUE" 
+FROM historial_d
