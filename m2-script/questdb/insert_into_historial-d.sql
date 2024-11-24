89cddac5517cfe2898887bc135d31b51d45cfb13
TerryLai422 <35938314+TerryLai422@users.noreply.github.com>
2024-11-24 01:42:14 -0500

    indicators

--- m2-script/questdb/insert_into_historial-d.sql
+++ m2-script/questdb/insert_into_historial-d.sql
@@ -1,16 +1,76 @@
 INSERT INTO historial_d
-SELECT TICKER, 
-PER, 
-to_timestamp(concat(DATE, ' ', TIME), 'yyyyMMdd HHmmss') AS DATE, 
-OPEN, 
-HIGH, 
-LOW, 
-CLOSE, 
-VOL, 
-OPENINT 
+SELECT 
+    TICKER, 
+    PER, 
+    to_timestamp(concat(DATE, ' ', TIME), 'yyyyMMdd HHmmss') AS DATE, 
+    OPEN, 
+    HIGH, 
+    LOW, 
+    CLOSE, 
+    VOL, 
+    OPENINT 
 FROM historial_raw_d;
 
-SELECT TICKER, PER, DATE, CLOSE, 
-avg(close) OVER (PARTITION BY TICKER, PER ORDER BY DATE ROWS BETWEEN 4 PRECEDING AND CURRENT ROW) 
-AS MovingAvg_5Day 
+SELECT 
+    TICKER, 
+    'MA_5', 
+    DATE, 
+    CLOSE, 
+    avg(close) 
+        OVER 
+        (PARTITION BY TICKER, PER ORDER BY DATE 
+        ROWS BETWEEN UNBOUNDED PRECEDING AND 4 PRECEDING) 
+        AS "VALUE" 
+FROM historial_d
+
+INSERT INTO indicators
+SELECT 
+    TICKER, 
+    'MA_20', 
+    DATE, 
+    CLOSE, 
+    avg(close) 
+        OVER 
+        (PARTITION BY TICKER, PER ORDER BY DATE 
+        ROWS BETWEEN UNBOUNDED PRECEDING AND 19 PRECEDING) 
+        AS "VALUE" 
+FROM historial_d
+
+INSERT INTO indicators
+SELECT 
+    TICKER, 
+    'MA_50', 
+    DATE, 
+    CLOSE, 
+    avg(close) 
+        OVER 
+        (PARTITION BY TICKER, PER ORDER BY DATE 
+        ROWS BETWEEN UNBOUNDED PRECEDING AND 49 PRECEDING) 
+        AS "VALUE" 
+FROM historial_d
+
+INSERT INTO indicators
+SELECT 
+    TICKER, 
+    'MA_150', 
+    DATE, 
+    CLOSE, 
+    avg(close) 
+        OVER 
+        (PARTITION BY TICKER, PER ORDER BY DATE 
+        ROWS BETWEEN UNBOUNDED PRECEDING AND 149 PRECEDING) 
+        AS "VALUE" 
+FROM historial_d
+
+INSERT INTO indicators
+SELECT 
+    TICKER, 
+    'MA_200', 
+    DATE, 
+    CLOSE, 
+    avg(close) 
+        OVER 
+        (PARTITION BY TICKER, PER ORDER BY DATE 
+        ROWS BETWEEN UNBOUNDED PRECEDING AND 199 PRECEDING) 
+        AS "VALUE" 
 FROM historial_d
