2afdd44a9c962c04aaf3592a6ae444ecdef9aec1
TerryLai422 <35938314+TerryLai422@users.noreply.github.com>
2024-11-24 23:08:55 -0500

    comparison

--- m2-script/questdb/comparison.sql
+++ m2-script/questdb/comparison.sql
@@ -7,8 +7,13 @@ SELECT
     i1.VALUE AS 'value1',
     i2.VALUE AS 'value2',
     i1.VALUE > i2.VALUE AS 'cross'
-FROM indicators i1, indicators i2
+FROM 
+    indicators i1, 
+    indicators i2
 WHERE 
-i1.TICKER = i2.TICKER AND i1.DATE = i2.DATE
-AND i1.TYPE = 'MA_50' AND i2.TYPE = 'MA_200'
-AND i1.VALID IS NOT NULL AND i2.VALID IS NOT NULL
+    i1.TICKER = i2.TICKER 
+    AND i1.DATE = i2.DATE
+    AND i1.TYPE = 'MA_50' 
+    AND i2.TYPE = 'MA_200'
+    AND i1.VALID IS NOT NULL 
+    AND i2.VALID IS NOT NULL
