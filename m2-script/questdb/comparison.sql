fbd570c33af62c809ff93d24684fa4fdb634d37f
TerryLai422 <35938314+TerryLai422@users.noreply.github.com>
2024-11-24 23:04:57 -0500

    comparison

--- m2-script/questdb/comparison.sql
+++ m2-script/questdb/comparison.sql
@@ -6,15 +6,9 @@ SELECT
     i2.TYPE AS 'type2',
     i1.VALUE AS 'value1',
     i2.VALUE AS 'value2',
-    CASE
-        WHEN i1.VALUE IS NULL OR I2.VALUE IS NULL THEN 'NA' 
-        ELSE
-            CASE  
-                WHEN i1.VALUE > i2.VALUE THEN 'A'
-                ELSE 'B'
-            END
-    END AS 'cross'
-from indicators i1, indicators i2
-where 
-i1.TICKER = i2.TICKER and i1.DATE = i2.DATE
-and i1.TYPE = 'MA_5' and i2.TYPE = 'MA_10'
+    i1.VALUE > i2.VALUE AS 'cross'
+FROM indicators i1, indicators i2
+WHERE 
+i1.TICKER = i2.TICKER AND i1.DATE = i2.DATE
+AND i1.TYPE = 'MA_50' AND i2.TYPE = 'MA_200'
+AND i1.VALID IS NOT NULL AND i2.VALID IS NOT NULL
