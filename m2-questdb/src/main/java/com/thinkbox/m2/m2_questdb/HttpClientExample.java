6e9020e09d56e29104f2804aa2dbceff94f5e0c8
TerryLai422 <35938314+TerryLai422@users.noreply.github.com>
2024-11-29 00:40:09 -0500

    read files from path

--- m2-questdb/src/main/java/com/thinkbox/m2/m2_questdb/HttpClientExample.java
+++ m2-questdb/src/main/java/com/thinkbox/m2/m2_questdb/HttpClientExample.java
@@ -8,17 +8,41 @@ import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
 
+import java.nio.file.FileVisitOption;
+import java.nio.file.FileVisitResult;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.ArrayList;
+import java.util.EnumSet;
+import java.util.List;
+import java.io.IOException;
 import java.io.File;
 
 public class HttpClientExample {
     public static void main(String[] args) {
-        String url = "http://192.168.51.230:9000/imp?fmt=json&forceHeader=true&name=historial_raw_d";
-        String fileName ="/Users/admin/aapl.us.txt";
+        Path startPath = Paths.get("/Users/admin/nasdaq_stocks");
+        String urlTemplate = "http://%s/imp?fmt=json&forceHeader=true&name=%s";
+        String hostName = "127.0.0.1:9000";
         String tableName = "historial_raw_d";
-        importFile(url, tableName, fileName);
+
+        String url = String.format(urlTemplate, hostName, tableName);
+
+        try {
+            List<String> fileNames = listFiles(startPath);
+            for (String fullFileName : fileNames) {
+                importFile(url, fullFileName);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
-    public static void importFile(String url, String tableName, String fileName) {
+
+    public static void importFile(String url, String fileName) {
+        System.out.println(fileName);
         File file = new File(fileName);
         try (CloseableHttpClient httpClient = HttpClients.createDefault()) {
             HttpPost uploadFile = new HttpPost(url);
@@ -39,5 +63,24 @@ public class HttpClientExample {
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+    public static List<String> listFiles(Path startPath) throws IOException {
+        List<String> fileNames = new ArrayList<>();
+        Files.walkFileTree(startPath, EnumSet.noneOf(FileVisitOption.class), Integer.MAX_VALUE, new SimpleFileVisitor<Path>() {
+            @Override
+            public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+                if (!file.getFileName().startsWith(".DS_Store")) {
+                    fileNames.add(file.toString());
+                }
+                return FileVisitResult.CONTINUE;
+            }
+
+            @Override
+            public FileVisitResult visitFileFailed(Path file, IOException exc) throws IOException {
+                System.err.println("Failed to visit file: " + file.toString() + " (" + exc.getMessage() + ")");
+                return FileVisitResult.CONTINUE;
+            }
+        });
+        return fileNames;
     }
 }
