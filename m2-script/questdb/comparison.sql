1d2635fa671fd784dedee55b4a984454feb1d94c
TerryLai422 <35938314+TerryLai422@users.noreply.github.com>
2024-12-02 00:59:13 -0500

    update case

--- m2-script/questdb/comparison.sql
+++ m2-script/questdb/comparison.sql
@@ -2,11 +2,14 @@ SELECT
     i1.ticker, 
     i1.date,
     i1.close, 
-    i1.TYPE AS 'type1', 
-    i2.TYPE AS 'type2',
-    i1.VALUE AS 'value1',
-    i2.VALUE AS 'value2',
-    i1.VALUE > i2.VALUE AS 'cross'
+    i1.type AS 'type1', 
+    i2.type AS 'type2',
+    i1.value AS 'value1',
+    i2.value AS 'value2',
+    CASE  
+        WHEN i1.value > i2.value THEN 'A'
+        ELSE 'B'
+    END AS 'cross'
 FROM 
     indicators i1, 
     indicators i2
