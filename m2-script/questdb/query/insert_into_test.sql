372d95d9b7d9acdfc25c2bf68383fe512d66d10d
TerryLai422 <35938314+TerryLai422@users.noreply.github.com>
2024-12-09 00:36:51 -0500

    Update insert_into_test.sql

--- m2-script/questdb/query/insert_into_test.sql
+++ m2-script/questdb/query/insert_into_test.sql
@@ -1,2 +1,48 @@
 insert into test VALUES
 ('2024-09-12T00:00:00.000000Z', -4, 0)
+
+
+WITH first_stage AS
+(SELECT 
+    ticker, 
+    'MA_5' AS type, 
+    date, 
+    close, 
+    avg(close) 
+        OVER 
+        (PARTITION BY ticker ORDER BY date 
+        ROWS BETWEEN 4 PRECEDING AND CURRENT ROW) 
+        AS 'value',    
+    first_value(close) 
+        OVER 
+        (PARTITION BY ticker ORDER BY date 
+        ROWS BETWEEN 4 PRECEDING AND CURRENT ROW) 
+        AS 'first',
+    count() 
+        OVER 
+        (PARTITION BY ticker ORDER BY date 
+        ROWS BETWEEN UNBOUNDED PRECEDING AND 4 PRECEDING) 
+        AS 'total'        
+FROM historial_d
+WHERE ticker = 'AAPL'
+),
+second_stage AS
+(SELECT     
+    type,
+    date,
+    ticker,
+    close,
+    value,
+    first,    
+    total,
+    close - value AS 'difference',
+    ((close - value) / value) * 100 AS 'percentage',
+    first_value(close - value) OVER (
+        PARTITION BY ticker        
+        ORDER BY date
+        ROWS 1 PRECEDING EXCLUDE CURRENT ROW
+    ) AS price_from_1_row_before,
+FROM first_stage 
+)
+SELECT * FROM second_stage
+WHERE TOTAL > 0;
