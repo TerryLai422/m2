6a1a5ccce9d5b870202166b8a45546770345d4a3
TerryLai422 <35938314+TerryLai422@users.noreply.github.com>
2024-12-11 00:34:33 -0500

    update mv20 > mv50 comparison

--- m2-script/questdb/query/comparison1.sql
+++ m2-script/questdb/query/comparison1.sql
@@ -1,21 +1,8 @@
 SELECT 
-    i1.ticker, 
-    i1.date,
-    i1.close, 
-    i1.TYPE AS 'type1', 
-    i2.TYPE AS 'type2',
-    i1.VALUE AS 'value1',
-    i2.VALUE AS 'value2',
-    CASE
-        WHEN i1.VALUE IS NULL OR I2.VALUE IS NULL THEN 'NA' 
-        ELSE
-            CASE  
-                WHEN i1.VALUE > i2.VALUE THEN 'A'
-                ELSE 'B'
-            END
-    END AS 'cross'
-from indicators i1, indicators i2
-where 
-i1.ticker = i2.ticker and i1.date = i2.date
-and i1.type = 'MA_50' and i2.type = 'MA_200'
-ORDER BY i1.date DESC
+    i1.date, i1.ticker, 
+    i1.value2 AS 'value1', 
+    i2.value2 AS 'value2',
+FROM indicators i1
+JOIN indicators i2 ON i1.date = i2.date AND i1.ticker = i2.ticker
+WHERE i1.type = 'MV_20' and i2.type = 'MV_50'
+AND i1.ticker = 'AAPL' order by date desc
