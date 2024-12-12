5b52425fd9e5a2c152dcb174bfc61a0ea9df4ea8
TerryLai422 <35938314+TerryLai422@users.noreply.github.com>
2024-12-12 00:41:59 -0500

    update ticker_info

---
+++ m2-script/questdb/query/update_ticker_info.sql
@@ -0,0 +1,2 @@
+UPDATE ticker_info SET sector = 'Technology', industry = 'Consumer Electronics' WHERE ticker = 'FOXX';
+UPDATE ticker_info SET sector = 'Healthcare', industry = 'Biotechnology' WHERE ticker = 'TLX';
